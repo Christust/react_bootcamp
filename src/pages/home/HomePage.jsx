5ab533d646af3e48277168b6e4940da4c7056991
Christos Marroquin <christos.marroquin@hotmail.com>
2023-03-19 21:14:09 -0600

    prueba

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -7,6 +7,7 @@ const HomePage = () => {
       <navbar>
         <Link to={"/"}>Home</Link>
         <Link to={"task-list"}>Task List</Link>
+        .
       </navbar>
       <Outlet></Outlet>
     </div>
